4b35647c555c901f5e3549ca4ac8ad3100a6b46a
Attila Deak <adeak@bethel.jw.org>
2023-10-31 10:03:12 +0100

    keyboard changes

---
+++ keyboards/redox/keymaps/ad/config.h
@@ -0,0 +1,41 @@
+/*
+Copyright 2023 Attila Deák
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+/* Select hand configuration */
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+// #undef RGBLED_NUM
+// #define RGBLED_NUM 14
+// #define RGBLIGHT_HUE_STEP 8
+// #define RGBLIGHT_SAT_STEP 8
+// #define RGBLIGHT_VAL_STEP 8
+// #define RGBLIGHT_SLEEP
+
+/* Set Unicode mode by default */
+#define UNICODE_SELECTED_MODES UNICODE_MODE_MACOS, UNICODE_MODE_LINUX, UNICODE_MODE_WINCOMPOSE
+
+/* Change the default tapping toggle value (normally 5) */
+#define TAPPING_TOGGLE 2
+
+#define TAPPING_TERM 400
+
+// #define RETRO_TAPPING
+// #define IGNORE_MOD_TAP_INTERRUPT
---
+++ keyboards/redox/keymaps/ad/keymap.c
@@ -0,0 +1,275 @@
+/*
+Copyright 2023 Attila Deák
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+#define _QWERTY 0
+#define _NAV 1
+#define _SYMB 2
+#define _INTER 3
+#define _ADJUST 4
+
+
+enum custom_keycodes {
+    QWERTY = SAFE_RANGE,
+    NAV,
+    SYMB,
+    INTER,
+    ADJUST,
+    // These use process_record_user()
+    M_BRACKET_LEFT,
+    M_BRACKET_RIGHT,
+    SHRUG,
+    WAVE,
+    YOSHI,
+    THUMB_UP
+};
+
+// Shortcut to make keymap more readable
+#define KC_SYQT LT(_SYMB, KC_QUOT)
+#define NAV_L   MO(_NAV)
+#define MO_INTR MO(_INTER)
+#define TT_ADJ  TT(_ADJUST)
+
+// Non-breaking space.
+#define UC_00A0 UC(0x00a0)
+
+// // Tap dance keycodes
+enum tap_dance{
+  UNDO,
+  CUT,
+  COPY,
+  PASTE,
+  BOLD
+//   _CUT, // cut
+//   _COPY, // copy
+//   _PAST // paste
+};
+
+// // Tap Dance definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for KC, twice for Command
+    [UNDO] = ACTION_TAP_DANCE_DOUBLE(KC_Z, C(KC_Z)),
+    [CUT] = ACTION_TAP_DANCE_DOUBLE(KC_X, C(KC_X)),
+    [COPY] = ACTION_TAP_DANCE_DOUBLE(KC_C, C(KC_C)),
+    [PASTE] = ACTION_TAP_DANCE_DOUBLE(KC_V, C(KC_V)),
+    [BOLD] = ACTION_TAP_DANCE_DOUBLE(KC_B, C(KC_B)),
+//     [_CUT] = ACTION_TAP_DANCE_DOUBLE(KC_X, KC_CUT),
+//     [_COPY] = ACTION_TAP_DANCE_DOUBLE(KC_C, KC_COPY),
+//     [_PAST] = ACTION_TAP_DANCE_DOUBLE(KC_V, KC_PASTE)
+// };
+
+// Is shift being held? Let's store this in a bool.
+static bool shift_held = false;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+
+        case SHRUG:
+            if (record->event.pressed) {
+                send_unicode_string("(shrug)");
+            }
+            return false;
+            break;
+
+        case YOSHI:
+            if (record->event.pressed) {
+                SEND_STRING("(huggingface)");
+            }
+            return false;
+            break;
+
+        case THUMB_UP:
+            if (record->event.pressed) {
+                SEND_STRING("(thumbsup)");
+            }
+            return false;
+            break;
+
+        case WAVE:
+            if (record->event.pressed) {
+                SEND_STRING("(wave)");
+            }
+            return false;
+            break;
+
+        case KC_LSFT:
+            shift_held = record->event.pressed;
+            return true;
+            break;
+
+        case KC_RSFT:
+            shift_held = record->event.pressed;
+            return true;
+            break;
+
+        case M_BRACKET_LEFT: {
+            if (record->event.pressed) {
+                if (shift_held) {
+                    unregister_code(KC_LSFT);
+                    unregister_code(KC_RSFT);
+                    register_code(KC_LBRC);
+                } else {
+                    register_code(KC_LSFT);
+                    register_code(KC_9);
+                }
+            } else {  // Release the key
+                unregister_code(KC_LBRC);
+                unregister_code(KC_LSFT);
+                unregister_code(KC_RSFT);
+                unregister_code(KC_9);
+            }
+            return false;
+            break;
+        }
+
+        case M_BRACKET_RIGHT: {
+            if (record->event.pressed) {
+                if (shift_held) {
+                    unregister_code(KC_LSFT);
+                    unregister_code(KC_RSFT);
+                    register_code(KC_RBRC);
+                } else {
+                    register_code(KC_LSFT);
+                    register_code(KC_0);
+                }
+            } else {  // Release the key
+                unregister_code(KC_RBRC);
+                unregister_code(KC_LSFT);
+                unregister_code(KC_RSFT);
+                unregister_code(KC_0);
+            }
+            return false;
+            break;
+        }
+
+        case UNDO:
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(C(KC_Z)); // Intercept hold function to send Ctrl-Z
+                return false;
+            }
+            return true;             // Return true for normal processing of tap keycode
+
+        case CUT:
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(C(KC_X)); // Intercept hold function to send Ctrl-X
+                return false;
+            }
+            return true;             // Return true for normal processing of tap keycode
+
+        case COPY:
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(C(KC_C)); // Intercept hold function to send Ctrl-C
+                return false;
+            }
+            return true;             // Return true for normal processing of tap keycode
+
+        case PASTE:
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(C(KC_V)); // Intercept hold function to send Ctrl-V
+                return false;
+            }
+            return true;             // Return true for normal processing of tap keycode
+
+        case BOLD:
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(C(KC_B)); // Intercept hold function to send Ctrl-B
+                return false;
+            }
+            return true;             // Return true for normal processing of tap keycode
+
+    }
+    return true;
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [_QWERTY] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     KC_GRV  ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,                                            KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_MINS ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,KC_ESC  ,                          KC_PSCR ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_EQL  ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     NAV_L   ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,M_BRACKET_LEFT,            M_BRACKET_RIGHT ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_SYQT ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_LSFT ,C(KC_Z) ,C(KC_X) ,C(KC_C) ,C(KC_V) ,C(KC_B) ,TT(_NAV),KC_LSFT ,        KC_DEL  ,TT(_SYMB),KC_N   ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_RSFT ,
+  //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
+     MO_INTR ,KC_LCTL ,KC_HYPR ,KC_LALT ,     KC_LCMD ,    KC_SPC  ,KC_LCTL ,        KC_BSPC ,KC_ENT  ,    KC_LSFT ,     TT_ADJ  ,KC_APP  ,KC_BSLS ,MO_INTR
+  //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
+  ),
+
+  [_NAV] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     _______ ,KC_SLEP ,_______ ,_______ ,_______ ,_______ ,                                            _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,XXXXXXX ,KC_MPRV ,KC_MNXT ,KC_MPLY ,KC_MSTP ,_______ ,                          _______ ,KC_PGUP ,KC_HOME ,KC_END  ,KC_INS  ,KC_DEL  ,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,XXXXXXX ,KC_VOLD ,KC_VOLU ,KC_MUTE ,XXXXXXX ,_______ ,                          _______ ,KC_PGDN ,KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RIGHT,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_LSFT ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,_______ ,KC_LSFT ,        _______ ,_______ ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
+     _______ ,KC_LCTL ,KC_HYPR ,KC_LALT ,     KC_LCMD ,    _______ ,KC_LCTL ,        _______ ,XXXXXXX ,    XXXXXXX ,     _______ ,XXXXXXX ,XXXXXXX ,_______
+  //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
+  ),
+
+  [_SYMB] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     _______ ,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,                                            KC_F6   ,KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  ,KC_F11  ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     _______ ,KC_EXLM ,KC_AT   ,KC_HASH ,KC_DLR  ,KC_PERC ,_______ ,                          _______ ,KC_CIRC ,KC_AMPR ,KC_ASTR ,KC_MINS ,KC_UNDS ,KC_F12  ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     _______ ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,KC_LCBR ,                          KC_RCBR ,KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,_______ ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     _______ ,_______ ,_______ ,_______ ,_______ , _______ ,_______ ,_______ ,       _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,
+  //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
+     _______ ,_______ ,_______ ,_______ ,     _______ ,    _______ ,_______ ,        _______ ,_______ ,    _______ ,     _______ ,_______ ,_______ ,_______
+  //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
+  ),
+
+  [_INTER] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     XXXXXXX ,_______ ,_______ ,_______ ,_______ ,_______ ,                                            KC_CALC ,KC_BSPC ,KC_PSLS ,KC_PAST ,KC_PMNS ,_______ ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,XXXXXXX ,WAVE    ,YOSHI   ,XXXXXXX ,XXXXXXX ,_______ ,                          _______ ,XXXXXXX ,KC_P7   ,KC_P8   ,KC_P9   ,KC_PPLS ,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     _______ ,XXXXXXX ,XXXXXXX ,SHRUG   ,THUMB_UP,XXXXXXX ,_______ ,                          _______ ,XXXXXXX ,KC_P4   ,KC_P5   ,KC_P6   ,KC_PPLS ,_______ ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,_______ ,        _______ ,XXXXXXX ,XXXXXXX ,KC_P1   ,KC_P2   ,KC_P3   ,KC_PENT ,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
+     _______ ,XXXXXXX ,XXXXXXX ,XXXXXXX ,     XXXXXXX ,    UC_00A0 ,_______ ,        _______ ,_______ ,    _______ ,     KC_P0   ,KC_PDOT ,KC_PENT ,_______
+  //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
+  ),
+
+  [_ADJUST] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     EE_CLR  ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,                                            XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,QK_BOOT ,DB_TOGG ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,                          XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     _______ ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,_______ ,                          _______ ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,_______ ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,_______ ,        _______ ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
+     _______ ,XXXXXXX ,XXXXXXX ,XXXXXXX ,     XXXXXXX ,    XXXXXXX ,_______ ,        _______ ,XXXXXXX ,    XXXXXXX ,     _______ ,XXXXXXX ,XXXXXXX ,_______
+  //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
+  )
+
+};
